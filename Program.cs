98ae6971c2c1b6f01c3df5b19b2b02dd28d89a8b
gcardoso <ger9047@gmail.com>
2019-11-08 02:48:35 -0600

    Clases, objetos, atributos con C#

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+
+    class Escuela
+    {
+       public string nombre;
+
+        public string direccion;
+
+        public int añoFundacion;
+
+        public string CEO;
+
+        public void Timbrar()
+        {
+            //TODO
+            Console.Beep(20000, 3000); 
+
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var miEscuela = new Escuela();
+
+            miEscuela.nombre = "Platzi Academy";
+
+            miEscuela.direccion = "Cr 9 calle 72";
+
+            miEscuela.añoFundacion = 2012;
+
+            Console.WriteLine("TIMBRE");
+
+            miEscuela.Timbrar();
+
+            //Console.WriteLine("Hello World!");
+    
+        }
+    }
+}
